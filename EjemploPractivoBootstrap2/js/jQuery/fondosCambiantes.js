fb328f2fb886bd9d3e1a3c0d9a1a5ab8b24d3baf
Christofer Enríquez <christoferen7@gmail.com>
2013-07-16 22:03:42 -0600

    agregando el login
    Se hizo el login de una manera sencilla, la intensión es que se pueda
    migrar a phonegap esto e iniciar pruebas.

---
+++ EjemploPractivoBootstrap2/js/jQuery/fondosCambiantes.js
@@ -0,0 +1,27 @@
+/* 
+
+ */
+
+//$(document).on("ready", iniciando);
+//
+//function iniciando()
+//{
+//    $(document)(fondoCambianteAleatorio);
+//}
+//
+//function fondoCambianteAleatorio()
+//{
+//    var cantidadImagenes = 5;
+//    var fondoAleatorio = Math.round(Math.round(Math.random() * 100) / 20);//devuelve los valores de 0 a 5
+//    
+//    alert(fondoAleatorio);
+////    var colocandoFondo = ;
+//}
+//
+//function colocandoImagenes()
+//{
+//    var imagen;
+//    $(imagen).addClass("body", "html");
+//    
+//    alert(imagen);
+//}
